16bf19d54e3718937ad025bdc9197c735dd524aa
AlejandroRDev <iron-man@avengers.com>
2021-12-28 12:32:37 +0100

    Node Declaration and value assignement

---
+++ script.js
@@ -0,0 +1,82 @@
+//Nodos
+
+let result$$ = document.getElementById('result');
+const segm$$ = document.getElementById('segm');
+const segM$$ = document.getElementById('segM');
+const segA$$ = document.getElementById('segA');
+const term$$ = document.getElementById('term');
+const terM$$ = document.getElementById('terM');
+const cuad$$ = document.getElementById('cuad');
+const cuaJ$$ = document.getElementById('cuaJ');
+const cuaA$$ = document.getElementById('cuaA');
+const quid$$ = document.getElementById('quid');
+const quiJ$$ = document.getElementById('quiJ');
+const quiA$$ = document.getElementById('quiA');
+const sexm$$ = document.getElementById('sexm');
+const sexM$$ = document.getElementById('sexM');
+const sexA$$ = document.getElementById('sexA');
+const sepd$$ = document.getElementById('sepd');
+const sepm$$ = document.getElementById('sepm');
+const sepM$$ = document.getElementById('sepM');
+const oct$$ = document.getElementById('oct');
+const sum$$ = document.getElementById('sum');
+const chord$$ = document.getElementById('chord');
+const reset$$ = document.getElementById('reset');
+
+//valor de los botones de la calculadora
+
+segm$$.onclick = () => {
+    result$$.innerHTML = `0.5`;
+};
+segM$$.onclick = () => {
+    result$$.textContent = `1`;
+};
+segA$$.onclick = () => {
+    result$$.innerHTML = `1.5`;
+};
+term$$.onclick = () => {
+    result$$.innerHTML = `1.5`;
+};
+terM$$.onclick = () => {
+    result$$.innerHTML = `2`;
+};
+cuad$$.onclick = () => {
+    result$$.innerHTML = `2`;
+};
+cuaJ$$.onclick = () => {
+    result$$.innerHTML = `2.5`;
+};
+cuaA$$.onclick = () => {
+    result$$.innerHTML = `3`;
+};
+quid$$.onclick = () => {
+    result$$.innerHTML = `3`;
+};
+quiJ$$.onclick = () => {
+    result$$.innerHTML = `3.5`;
+};
+quiA$$.onclick = () => {
+    result$$.innerHTML = `4`;
+};
+sexm$$.onclick = () => {
+    result$$.innerHTML = `4`;
+};
+sexM$$.onclick = () => {
+    result$$.innerHTML = `4.5`;
+};
+sexA$$.onclick = () => {
+    result$$.innerHTML = `5`;
+};
+sepd$$.onclick = () => {
+    result$$.innerHTML = `4.5`;
+};
+sepm$$.onclick = () => {
+    result$$.innerHTML = `5`;
+};
+sepM$$.onclick = () => {
+    result$$.innerHTML = `5.5`;
+};
+oct$$.onclick = () => {
+    result$$.innerHTML = `6`;
+};
+
